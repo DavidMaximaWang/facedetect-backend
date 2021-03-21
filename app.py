c9dc8197db3446a77f425f5c25f9a139b1202bfb
qunlingwang@gmail.com <qunlingwang>
2021-03-21 15:01:44 -0700

    added face detect

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask import Flask, abort, jsonify, request, send_from_directory
 from flask_cors import CORS
 from flask_json import FlaskJSON, JsonError, as_json, json_response
 
+import faces
 from predict_image_classification import \
     predict_image_classification_tom_cluise
 
@@ -16,8 +17,10 @@ if not os.path.exists(UPLOAD_DIRECTORY):
 
 
 api = Flask(__name__)
-CORS(api)
+
 FlaskJSON(api)
+api.debug = os.environ.get('face_debug', True)
+CORS(api)
 
 @api.route("/files")
 def list_files():
@@ -44,20 +47,22 @@ def post_file():
         if imghdr.what(file) is None:
             raise "not an image"
    
-        filename = str(uuid.uuid4()) + ".jpg"
-        files.append({"filename": filename})
-        with open(os.path.join(UPLOAD_DIRECTORY, filename), "wb") as fp:
-            file.save(os.path.join(UPLOAD_DIRECTORY, filename))
+        filename = str(uuid.uuid4())
+        file_type = ".jpg"
+        files.append({"filename": filename + file_type, "detected_filename": filename + "_1" + file_type})
+        input_filename = os.path.join(UPLOAD_DIRECTORY, filename + file_type)
+        with open(input_filename, "wb") as fp:
+            file.save(input_filename)
             a = predict_image_classification_tom_cluise(
                 project="915709539634",
                 endpoint_id="529700721797365760",
                 location="us-central1",
-                filename=os.path.join(UPLOAD_DIRECTORY, filename)
+                filename=input_filename
             )
             files[-1] = {**files[-1], **a}
-
+            faces.main(input_filename, os.path.join(UPLOAD_DIRECTORY, filename + "_1" + file_type), 4)
 
     return json_response(data=files)
 
 if __name__ == "__main__":
-    api.run(host='0.0.0.0', debug=True, port=5000)
+    api.run(host='0.0.0.0', port=5000)
